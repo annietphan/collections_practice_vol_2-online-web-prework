866b6833b03d11fb63edd051faacfd3a515feeb6
Annie Phan <anniephan.atp@gmail.com>
2019-03-03 05:23:34 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -47,9 +47,7 @@ def organize_schools(array)
   location_h = {}
   array.collect {|key, value| location_h[value[:location]] = []}
   location_h.each do |key, value|
-    array.each do |key1, value1|
-      if key == value1[:location]then value << key1
-      end
+    array.each{|key1, value1| if key == value1[:location]then value << key1 end}
   end
 
   end
